bea731087f94ab6c2d930bacb7943f23b24cbd46
Tongbram <rtongbram@paypal.com>
2015-02-12 22:03:47 -0800

    adding bit reversing

--- kode/src/main/java/com/home/api/MathService.java
+++ kode/src/main/java/com/home/api/MathService.java
@@ -20,7 +20,24 @@ public class MathService{
         //int n = 5;
         //System.out.println("n="+isPrime(n));
         //double x = Math.pow(n, n);
-        callCheckForPair();
+        testReversetBits();
+    }
+    
+    private static void testReversetBits(){
+        int input = 5;
+        
+        int output = reverseBits(input);
+        System.out.println(" input = "+Integer.toBinaryString(input));
+        System.out.println(" output = "+Integer.toBinaryString(output));
+        //System.out.println(" output 2= "+Integer.toBinaryString(Integer.reverse(input)));
+        long input1 = 15L;
+        long output1 = reverse64Bits(input1);
+        System.out.println(" long input  = "+Long.toBinaryString(input1));
+        System.out.println(" long output = "+Long.toBinaryString(output1));
+    }
+    private static void callUtopian(){
+        System.out.println("utopian at 4 ="+utopianTree(4));
+        System.out.println("utopian at 3 ="+utopianTree(3));
     }
     
     private static void callCheckForPair(){
@@ -32,6 +49,55 @@ public class MathService{
         System.out.println("Result ="+result);
     }
     
+    
+    /**
+     * a tree of height 1 is given
+     * the number of growth cycle is given as times.
+     * First growth cycle doubles the height.
+     * The next growth cycle increments the height by 1;
+     * 
+     */
+    private static int utopianTree(int times){
+        int result = 1;
+        for(int i = 1; i<=times; i++){
+            if(i % 2 == 1){
+                result *=2;
+            }
+            else{
+                result++;
+            }
+        }
+        return result;
+    }
+    
+    private static void callFlipBits(){
+        int result = flipBits(2147483647);
+        System.out.println("2147483647 =>"+flipBits(2147483647));
+        System.out.println("1 =>"+flipBits(1));
+        System.out.println("0 =>"+flipBits(0));
+    }
+    
+    /**
+     * Flips all the bits of the incoming data
+     *
+     */
+    private static int flipBits(int data){
+        int result = data ^ 0xFFFFFFFF;
+        
+        return result;
+    }
+    /**
+     * in the given array , all numbers appear twice except for one.
+     **/
+    private static int lonelyinteger(int[] a) {
+
+        int result = 0;
+        for(int num : a){
+            result = result^num;
+        }
+        return result;
+
+    }
     /**
      * Given an array, find if 2 numbers in the array sum up to a value
      * 
@@ -149,5 +215,43 @@ public class MathService{
         }
         
         return true;
+    }
+    
+    public static int reverseBits(int num){
+        int a1 = 0x55555555, a2=0xAAAAAAAA; // ...0101 and ... 1010
+        int b1 = 0x33333333, b2=0xCCCCCCCC; // ...0011 and ... 1100
+        int c1 = 0x0F0F0F0F, c2=0xF0F0F0F0;// ... 00001111 and .. 11110000
+        int d1 = 0x00FF00FF, d2=0xFF00FF00; // .... 0000000011111111 and .. 1111111100000000
+        int e1 = 0x0000FFFF, e2=0xFFFF0000; 
+                
+        int result = num;
+        
+        result = ((result & a1) << 1) | ((result & a2) >> 1);
+        result = ((result & b1) << 2) | ((result & b2) >> 2);
+        result = ((result & c1) << 4) | ((result & c2) >> 4);
+        result = ((result & d1) << 8) | ((result & d2) >> 8);
+        result = ((result & e1) << 16) | ((result & e2) >> 16);
+        //System.out.println("55 ="+0x55);
+        return result;
+    }
+    
+    public static long reverse64Bits(long num){
+        long a1 = 0x5555555555555555L, a2=0xAAAAAAAAAAAAAAAAL; // ...0101 and ... 1010
+        long b1 = 0x3333333333333333L, b2=0xCCCCCCCCCCCCCCCCL; // ...0011 and ... 1100
+        long c1 = 0x0F0F0F0F0F0F0F0FL, c2=0xF0F0F0F0F0F0F0F0L;// ... 00001111 and .. 11110000
+        long d1 = 0x00FF00FF00FF00FFL, d2=0xFF00FF00FF00FF00L; // .... 0000000011111111 and .. 1111111100000000
+        long e1 = 0x0000FFFF0000FFFFL, e2=0xFFFF0000FFFF0000L; 
+        long f1 = 0x00000000FFFFFFFFL, f2=0xFFFFFFFF00000000L; 
+                
+        long result = num;
+        
+        result = ((result & a1) << 1) | ((result & a2) >> 1);
+        result = ((result & b1) << 2) | ((result & b2) >> 2);
+        result = ((result & c1) << 4) | ((result & c2) >> 4);
+        result = ((result & d1) << 8) | ((result & d2) >> 8);
+        result = ((result & e1) << 16) | ((result & e2) >> 16);
+        result = ((result & f1) << 32) | ((result & f2) >> 32);
+        //System.out.println("55 ="+0x55);
+        return result;
     }
 }
